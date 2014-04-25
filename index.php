a2d35f8b0cbfcd4d6da9cd5f8da9847c47138b12
umessh <funcky.umesh@gmail.com>
2014-04-26 02:42:16 +0530

    Variable API added bug fix Works

--- index.php
+++ index.php
@@ -140,7 +140,7 @@ $app->get('/variables/:type', function ($type) {
 
     	try {
   // open connection to MongoDB server 
-  $conn = MongoClient($_ENV['OPENSHIFT_MONGODB_DB_URL']);
+  $conn = new MongoClient($_ENV['OPENSHIFT_MONGODB_DB_URL']);
   // access database
   $db = $conn->myApp;
 
